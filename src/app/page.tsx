e1d49a895ee7cfd09374f777ec60ca1d5eee1510
Pokemon Go Coordinates <manavghiya@vishwajyot.com>
2025-12-06 10:48:26 +0530

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { fetchOpenAPISpec, parseEndpoints, groupByCategory } from '@/lib/openapi';
 import DocsPage from '@/components/DocsPage';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 3600;
-
 export default async function Home() {
   try {
     const spec = await fetchOpenAPISpec();
